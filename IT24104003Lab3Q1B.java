8656d402e68e4d4d49616ee5b3a86bd9b5b6e64f
Aathi682 <it24104003@my.sliit.lk>
2024-11-07 07:39:55 +0530

    Update IT24104003Lab3Q1B.java

--- IT24104003Lab3Q1B.java
+++ IT24104003Lab3Q1B.java
@@ -2,10 +2,12 @@ public class IT24104003Lab3Q1B{
     public static void main (String[]args)
     {
 
-    double totalAmount,discount,finalamount;
+    double totalAmount,discount,finalamount,riceperkg,kg;
 
+        riceperkg=225;
+        kg=5.0;
     
-    totalAmount = 1000.00;
+    totalAmount = riceperkg*kg;
 
     discount = totalAmount*0.10;
 
