60c5c261341db034a80048daf3a4ef0b20f04661
Aathi682 <it24104003@my.sliit.lk>
2024-11-08 13:27:37 +0530

    Add files via upload

--- IT24104003Lab3Q1A.java
+++ IT24104003Lab3Q1A.java
@@ -1,16 +1,21 @@
-public class IT24104003Lab3Q1A {
+import java.util.Scanner;
+
+public class It24104003Lab3Q1A{
     public static void main(String[] args)
    {
+Scanner sc= new Scanner(System.in);
+
+System.out.print("Enter the price of 1kg of rice: ");
+double riceperkg= sc.nextDouble();
+
+System.out.print("Enter the number of kilogram:");
+double kg= sc.nextDouble();
+
+ double finalprice = riceperkg*kg;
     
-     double finalprice,riceperKG,kg;
-    
-     riceperKG=225.00;
-     kg=5.00;
      
-     finalprice = riceperKG*kg;
-    
-     
-     System.out.println(finalprice);
+     System.out.println("finalprice; "+finalprice);
+sc.close();
      
     }
 }
